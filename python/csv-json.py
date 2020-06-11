89fe200da240d773944bac98c570be16bed38d6e
robmcelhinney <robmcelhinney@hotmail.com>
2020-06-12 00:13:50 +0100

    Update Readme, tidy up python and json

--- python/csv-json.py
+++ python/csv-json.py
@@ -13,13 +13,12 @@ def main():
     for obj in data:
         # print("obj: ", obj)
 
-
         new_obj = {}
 
         if ('birth' not in obj['fields'] or 'death' not in  obj['fields']):
             print("no birth or death. name: ", obj['fields']['name'])
             continue
-
+        
 
         birth = parser.parse(obj['fields']['birth'])
         death = parser.parse(obj['fields']['death'])
@@ -29,8 +28,7 @@ def main():
 
         new_obj['index'] = obj['fields']['index']
         new_obj['name'] = obj['fields']['name']
-        new_obj['age'] = abs(difference_in_years)
-
+        new_obj['cause'] = obj['fields']['cause']
 
         if ('reign_start' not in obj['fields'] or 'reign_end' not in obj['fields']):
             print("no reign_start or end. name: ", obj['fields']['name'])
@@ -41,16 +39,23 @@ def main():
 
         age_at_end_reign = relativedelta(reign_end, birth).years
         age_at_reign = relativedelta(reign_start, birth).years
-        # print("difference_in_years: ", difference_in_years)
+        reign_length = relativedelta(reign_end, reign_start).years
+        end_reign_to_death = relativedelta(death, reign_end).years
+        end_reign_to_death_months = relativedelta(death, reign_end).months
 
-        new_obj['reign_start'] = abs(age_at_reign)
-        new_obj['reign_end'] = abs(age_at_end_reign)
+        new_obj['Pre Emperor'] = abs(age_at_reign)
+        if reign_length == 0:
+            reign_length = 1
+        new_obj['Emperor'] = abs(reign_length)
+        if end_reign_to_death == 0 and end_reign_to_death_months != 0:
+            end_reign_to_death = 1
+        new_obj['Post Emperor'] = abs(end_reign_to_death)
 
         new_json.append(new_obj)
 
         # print("new_obj: ", new_obj)
 
-    with open('emperorRedone.json', 'w') as f:
+    with open('../src/data/emperors.json', 'w') as f:
         json.dump(new_json, f)
 
 
